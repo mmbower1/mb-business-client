660d8d7237c802a2c71312fd3583e9e784930990
mmbower1 <mttbwr91@gmail.com>
2025-04-06 21:11:55 -0700

    navbar, mobile nav, homepage, about page

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import "./index.css";
 
 // components
 import { LoadingScreen } from "./components/LoadingScreen";
+import { MobileNav } from "./components/MobileNav";
 import { Navbar } from "./components/Navbar";
+
+import { About } from "./components/sections/About";
+import { Home } from "./components/sections/Home";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
@@ -16,7 +22,10 @@ function App() {
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
-        <Navbar />
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileNav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
+        <About />
       </div>
     </>
   );
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState();
-  const fullText = "<Loading... />";
+  const fullText = "Loading vite app...";
 
   useEffect(() => {
     let index = 0;
