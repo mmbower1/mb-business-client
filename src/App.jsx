fcc792a9d2470a92bdd34f1cccb4fc6f3cfd42f6
mmbower1 <mttbwr91@gmail.com>
2025-11-21 10:42:13 -0800

    background gradient

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <div
         className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
-        } bg-black text-gray-100`}
+        } bg-gradient-to-tl from-blue-950 to-black w-full text-gray-100`}
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileNav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
