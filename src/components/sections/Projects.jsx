c0b6e90995f5fad07c81083217c4a51a22a607c6
mmbower1 <mttbwr91@gmail.com>
2025-11-22 13:59:09 -0800

    added american arbor project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -35,16 +35,18 @@ export const Projects = () => {
               </p>
 
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Firebase", "SCSS", "Vercel"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                {["React", "Firebase", "Tailwind", "Vercel"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
                   hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
                   "
-                  >
-                    {tech}
-                  </span>
-                ))}
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
               </div>
               <div className="flex justify-between items-center">
                 <a
@@ -88,15 +90,15 @@ export const Projects = () => {
               </div>
             </div>
             <div className="p-6 rounded-xl border border-white/20 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-              <h3 className="text-xl font-bold mb-2">Accent Legal</h3>
+              <h3 className="text-xl font-bold mb-2">American Arbor</h3>
               <p className="text-gray-400 mb-4"></p>
               <p>
-                A business site built with the MERN stack intended for
-                immigrants to seek appointments and the information they need
-                under legal authority in the United States.
+                A freelance website built for a close friends' Horticulture Tree
+                Arbor business for Davis and Sacramento region. Scaled with
+                Vite, React, Scss, Firebase and Vercel.
               </p>
               <div className="flex flex-wrap gap-2">
-                {["React", "NodeJS", "MongoDB", "Vercel"].map((tech, key) => (
+                {["React", "Scss", "Firebase", "Vercel"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -109,38 +111,9 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="https://accentlegal-client.vercel.app/"
+                  href="https://american-arbor.vercel.app/"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
-                  View Project →
-                </a>
-              </div>
-            </div>
-            <div className="p-6 rounded-xl border border-white/20 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-              <h3 className="text-xl font-bold mb-2">NBA API</h3>
-              <p className="text-gray-400 mb-4"></p>
-              <p>
-                If you're an NBA fan, get all current roster info needed using
-                this free NBA application programming interface source.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Javascript", "", "AWS"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                  hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
-                  "
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              <div className="flex justify-between items-center">
-                <a
-                  href="https://nba-app.vercel.app/"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                >
-                  {" "}
                   View Project →
                 </a>
               </div>
