9162cc1da5dd69ee133ad69ed20c0d2767d15cf1
mmbower1 <mttbwr91@gmail.com>
2026-07-06 11:05:19 -0700

    added pearl dating app to profile

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -25,6 +25,39 @@ export const Projects = () => {
             Featured Projects
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        <div className="p-6 rounded-xl border border-white/20 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+              <h3 className="text-xl font-bold mb-2">Pearl Dating </h3>
+              <p className="text-gray-400 mb-4"></p>
+              <p>
+                My first patented project, I built a dating app with MERN + Typescript. What makes this app different is
+                that swiping gets disabled once you have gained a match. Along with this, there is an accountability score
+                to prevent bad behaviors from users. Rather than endless swiping, lets make dating how it should be again. 
+              </p>
+
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Firebase", "Tailwind", "Vercel"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                  hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                  "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://dating-app-pearl-nine.vercel.app/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  {" "}
+                  View Project →
+                </a>
+              </div>
+            </div>
             <div className="p-6 rounded-xl border border-white/20 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
               <h3 className="text-xl font-bold mb-2">Eagles Ecommerce</h3>
               <p className="text-gray-400 mb-4"></p>
